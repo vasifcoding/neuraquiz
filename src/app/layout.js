0af6b535711a7a8e7d4c6e919ad82669651207c3
vasifcoding <vaska.blogger111@gmail.com>
2025-06-10 01:11:48 +0300

    vercel analytics eklendi

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
+import { Analytics } from "@vercel/analytics/next"
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -23,7 +23,7 @@ export default function RootLayout({ children }) {
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
-
+<Analytics/>
       </body>
     </html>
   );
